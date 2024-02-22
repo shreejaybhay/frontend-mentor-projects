82840aeddfd8ef32811a46e46b7e4196ecaf2f60
Shree Jaybhay <shreejaybhay26@gmail.com>
2024-02-22 02:49:15 -0500

    faq-accordion

---
+++ frontend-projects/faq-accordion/script.js
@@ -0,0 +1,65 @@
+// let plusbtn = document.getElementById("plusbtn");
+// let minusbtn = document.getElementById("minusbtn");
+// let pdiv = document.getElementById("pdiv");
+
+// let hide = 0;
+
+// plusbtn.addEventListener("click", function () {
+//   if (hide == 0) {
+//     plusbtn.style.display = "none";
+//     minusbtn.style.display = "block";
+//     hide = 1;
+//   } else {
+//     plusbtn.style.display = "block";
+//     minusbtn.style.display = "none";
+//     hide = 0;
+//   }
+// });
+
+function onbtn() {
+  document.getElementById("pdiv").style.display = "block";
+  document.getElementById("plusbtn").style.display = "none";
+  document.getElementById("minusbtn").style.display = "block";
+}
+
+function ofbtn() {
+  document.getElementById("pdiv").style.display = "none";
+  document.getElementById("plusbtn").style.display = "block";
+  document.getElementById("minusbtn").style.display = "none";
+}
+
+function onbtn1() {
+  document.getElementById("pdiv1").style.display = "block";
+  document.getElementById("plusbtn1").style.display = "none";
+  document.getElementById("minusbtn1").style.display = "block";
+}
+
+function ofbtn1() {
+  document.getElementById("pdiv1").style.display = "none";
+  document.getElementById("plusbtn1").style.display = "block";
+  document.getElementById("minusbtn1").style.display = "none";
+}
+
+function onbtn2() {
+  document.getElementById("pdiv2").style.display = "block";
+  document.getElementById("plusbtn2").style.display = "none";
+  document.getElementById("minusbtn2").style.display = "block";
+}
+
+function ofbtn2() {
+  document.getElementById("pdiv2").style.display = "none";
+  document.getElementById("plusbtn2").style.display = "block";
+  document.getElementById("minusbtn2").style.display = "none";
+}
+
+function onbtn3() {
+  document.getElementById("pdiv3").style.display = "block";
+  document.getElementById("plusbtn3").style.display = "none";
+  document.getElementById("minusbtn3").style.display = "block";
+}
+
+function ofbtn3() {
+  document.getElementById("pdiv3").style.display = "none";
+  document.getElementById("plusbtn3").style.display = "block";
+  document.getElementById("minusbtn3").style.display = "none";
+}
